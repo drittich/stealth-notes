1b68ef87c045a33092e46ece70b6fbf98ec9cd63
D'Arcy Rittich <drittich@gmail.com>
2021-01-10 12:38:05 -0500

    tweak

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -154,7 +154,7 @@ namespace StealthNotes
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(13, 17);
+			this.label1.Location = new System.Drawing.Point(13, 13);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(317, 25);
 			this.label1.TabIndex = 9;
